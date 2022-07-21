834515acda6fda68349a55081a50448812a89ad1
ryanmerolle <ryan.merolle@gmail.com>
2022-07-21 04:17:49 +0000

    correct constants

--- netbox_access_lists/constants.py
+++ netbox_access_lists/constants.py
@@ -5,7 +5,7 @@ from django.db.models import Q
 
 ACL_HOST_ASSIGNMENT_MODELS = Q(
     Q(app_label='dcim', model='device') |
-#    Q(app_label='dcim', model='virtualchassis') |
+    Q(app_label='dcim', model='virtualchassis') |
     Q(app_label='virtualization', model='virtualmachine')
 )
 

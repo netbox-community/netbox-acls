af6d2d67ad6157627805acbef9e1f6065818b2ac
ryanmerolle <ryan.merolle@gmail.com>
2022-07-21 04:18:40 +0000

    correct missing view

--- netbox_access_lists/views.py
+++ netbox_access_lists/views.py
@@ -12,6 +12,7 @@ __all__ = (
     'AccessListView',
     'AccessListListView',
     'AccessListEditView',
+    'AccessListDeleteView',
     'ACLStandardRuleView',
     'ACLStandardRuleListView',
     'ACLStandardRuleEditView',

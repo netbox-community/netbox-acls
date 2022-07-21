a3892c143047524ed913a63318917640f1b83cc2
ryanmerolle <ryan.merolle@gmail.com>
2022-07-21 13:28:35 +0000

    removed broken prefetching in api views

--- netbox_access_lists/api/views.py
+++ netbox_access_lists/api/views.py
@@ -21,9 +21,7 @@ class AccessListViewSet(NetBoxModelViewSet):
     """
     Defines the view set for the django AccessList model & associates it to a view.
     """
-    queryset = models.AccessList.objects.prefetch_related(
-        'device', 'tags'
-    ).annotate(
+    queryset = models.AccessList.objects.prefetch_related('tags').annotate(
         rule_count=Count('aclextendedrules') + Count('aclstandardrules')
     )
     serializer_class = AccessListSerializer

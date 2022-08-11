d67500204b2d4a1088411c108e1cb31759d49a91
Abhimanyu Saharan <desk.abhimanyu@gmail.com>
2022-08-11 22:33:13 +0530

    fixes #54

--- netbox_access_lists/template_content.py
+++ netbox_access_lists/template_content.py
@@ -4,86 +4,86 @@ from extras.plugins import PluginTemplateExtension
 from .models import AccessList, ACLInterfaceAssignment
 
 __all__ = (
-    "AccessLists",
-    "ACLInterfaceAssignments",
-    "DeviceAccessLists",
-    "VirtualChassisAccessLists",
-    "VMAccessLists",
-    "DeviceACLInterfaceAssignments",
-    "VMAACLInterfaceAssignments",
+	"AccessLists",
+	"ACLInterfaceAssignments",
+	"DeviceAccessLists",
+	"VirtualChassisAccessLists",
+	"VMAccessLists",
+	"DeviceACLInterfaceAssignments",
+	"VMAACLInterfaceAssignments",
 )
 
 
 class ACLInterfaceAssignments(PluginTemplateExtension):
-    def right_page(self):
-        obj = self.context["object"]
+	def right_page(self):
+		obj = self.context["object"]
 
-        acl_interface_assignments = None
-        ctype = ContentType.objects.get_for_model(obj)
-        if ctype.model in ["interface", "vminterface"]:
-            acl_interface_assignments = ACLInterfaceAssignment.objects.filter(
-                assigned_object_id=obj.pk,
-                assigned_object_type=ctype,
-            )
+		acl_interface_assignments = None
+		ctype = ContentType.objects.get_for_model(obj)
+		if ctype.model in ["interface", "vminterface"]:
+			acl_interface_assignments = ACLInterfaceAssignment.objects.filter(
+				assigned_object_id=obj.pk,
+				assigned_object_type=ctype,
+			)
 
-        return self.render(
-            "inc/assigned_interface/access_lists.html",
-            extra_context={
-                "acl_interface_assignments": acl_interface_assignments,
-                "type": ctype.model
-                if ctype.model == "device"
-                else ctype.name.replace(" ", "_"),
-            },
-        )
+		return self.render(
+			"inc/assigned_interface/access_lists.html",
+			extra_context={
+				"acl_interface_assignments": acl_interface_assignments,
+				"type": ctype.model,
+				"parent_type": "device" if ctype.model == "interface" else "virtual_machine",
+				"parent_id": obj.device.pk if ctype.model == "interface" else obj.virtual_machine.pk
+			},
+		)
 
 
 class AccessLists(PluginTemplateExtension):
-    def right_page(self):
-        obj = self.context["object"]
+	def right_page(self):
+		obj = self.context["object"]
 
-        access_lists = None
-        ctype = ContentType.objects.get_for_model(obj)
-        if ctype.model in ["device", "virtualchassis", "virtualmachine"]:
-            access_lists = AccessList.objects.filter(
-                assigned_object_id=obj.pk,
-                assigned_object_type=ctype,
-            )
+		access_lists = None
+		ctype = ContentType.objects.get_for_model(obj)
+		if ctype.model in ["device", "virtualchassis", "virtualmachine"]:
+			access_lists = AccessList.objects.filter(
+				assigned_object_id=obj.pk,
+				assigned_object_type=ctype,
+			)
 
-        return self.render(
-            "inc/assigned_host/access_lists.html",
-            extra_context={
-                "access_lists": access_lists,
-                "type": ctype.model
-                if ctype.model == "device"
-                else ctype.name.replace(" ", "_"),
-            },
-        )
+		return self.render(
+			"inc/assigned_host/access_lists.html",
+			extra_context={
+				"access_lists": access_lists,
+				"type": ctype.model
+				if ctype.model == "device"
+				else ctype.name.replace(" ", "_"),
+			},
+		)
 
 
 class DeviceAccessLists(AccessLists):
-    model = "dcim.device"
+	model = "dcim.device"
 
 
 class VirtualChassisAccessLists(AccessLists):
-    model = "dcim.virtualchassis"
+	model = "dcim.virtualchassis"
 
 
 class VMAccessLists(AccessLists):
-    model = "virtualization.virtualmachine"
+	model = "virtualization.virtualmachine"
 
 
 class DeviceACLInterfaceAssignments(ACLInterfaceAssignments):
-    model = "dcim.interface"
+	model = "dcim.interface"
 
 
 class VMAACLInterfaceAssignments(ACLInterfaceAssignments):
-    model = "virtualization.vminterface"
+	model = "virtualization.vminterface"
 
 
 template_extensions = [
-    DeviceAccessLists,
-    VirtualChassisAccessLists,
-    VMAccessLists,
-    DeviceACLInterfaceAssignments,
-    VMAACLInterfaceAssignments,
+	DeviceAccessLists,
+	VirtualChassisAccessLists,
+	VMAccessLists,
+	DeviceACLInterfaceAssignments,
+	VMAACLInterfaceAssignments,
 ]

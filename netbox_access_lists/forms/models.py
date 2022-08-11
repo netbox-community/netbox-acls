ffad515df995d92451d04304e8b4235499fb1801
Abhimanyu Saharan <desk.abhimanyu@gmail.com>
2022-08-11 22:12:32 +0530

    fixes #50

--- netbox_access_lists/forms/models.py
+++ netbox_access_lists/forms/models.py
@@ -76,7 +76,7 @@ class AccessListForm(NetBoxModelForm):
         queryset=Device.objects.all(),
         required=False,
         query_params={
-            "region": "$region",
+            "region_id": "$region",
             "group_id": "$site_group",
             "site_id": "$site",
         },

19a63d024470479bd525e5e0a2fc1f5926823217
Ryan Merolle <ryanmerolle@users.noreply.github.com>
2022-10-07 14:41:47 -0400

    correct version

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ def read(rel_path):
 
 setup(
     name="netbox-acls",
-    version="1.1.1",
+    version="1.0.1",
     # version=get_version("netbox_acls/version.py"),
     description="A NetBox plugin for Access List management",
     # long_description=long_description,

66334e406e1173a2fcd4ced6ad4fe4463cca15f8
Abhimanyu Saharan <desk.abhimanyu@gmail.com>
2023-01-21 23:09:53 +0530

    fixed site and device filtering

--- netbox_acls/forms/filtersets.py
+++ netbox_acls/forms/filtersets.py
@@ -123,11 +123,15 @@ class ACLInterfaceAssignmentFilterForm(NetBoxModelFilterSetForm):
     site = DynamicModelChoiceField(
         queryset=Site.objects.all(),
         required=False,
+        query_params={
+            "region_id": "$region",
+            "group_id": "$site_group"
+        }
     )
     device = DynamicModelChoiceField(
         queryset=Device.objects.all(),
         query_params={
-            "region": "$region",
+            "region_id": "$region",
             "group_id": "$site_group",
             "site_id": "$site",
         },

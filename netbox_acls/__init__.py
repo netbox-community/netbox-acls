deeef54920880a1e6cb5fb7cb693edd4eab5dcee
Abhimanyu Saharan <desk.abhimanyu@gmail.com>
2023-01-22 00:20:41 +0530

    initial changes for nb 3.4

--- netbox_acls/__init__.py
+++ netbox_acls/__init__.py
@@ -11,6 +11,8 @@ class NetBoxAccessListsConfig(PluginConfig):
     description = "Manage simple ACLs in NetBox"
     version = "1.0.0"
     base_url = "access-lists"
+    min_version = "3.4.0"
+    max_version = "3.4.99"
 
 
 config = NetBoxAccessListsConfig

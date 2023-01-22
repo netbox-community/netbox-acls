211ff4fc7a5bd4af1f306d8ba5be91322aa96c10
ryanmerolle <ryan.merolle@gmail.com>
2023-01-21 23:04:43 -0500

    correct version and package details

--- setup.py
+++ setup.py
@@ -3,20 +3,29 @@ import os.path
 
 from setuptools import find_packages, setup
 
-with open("README.md") as fh:
+
+with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 
 def read(rel_path):
     here = os.path.abspath(os.path.dirname(__file__))
-    with codecs.open(os.path.join(here, rel_path), "r") as fp:
+    with codecs.open(os.path.join(here, rel_path), 'r') as fp:
         return fp.read()
+
+
+def get_version(rel_path):
+    for line in read(rel_path).splitlines():
+        if line.startswith('__version__'):
+            delim = '"' if '"' in line else "'"
+            return line.split(delim)[1]
+        else:
+            raise RuntimeError("Unable to find version string.")
 
 
 setup(
     name="netbox-acls",
-    version="1.1.0",
-    # version=get_version("netbox_acls/version.py"),
+    version=get_version("netbox_acls/version.py"),
     description="A NetBox plugin for Access List management",
     long_description=long_description,
     long_description_content_type="text/markdown",
@@ -25,4 +34,8 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     zip_safe=False,
+    classifiers=[
+        'Framework :: Django',
+        'Programming Language :: Python :: 3',
+    ]
 )

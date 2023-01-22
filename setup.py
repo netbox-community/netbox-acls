e948e7a7ed97458aff8078fff70308abd8c826e7
ryanmerolle <ryan.merolle@gmail.com>
2023-01-22 04:53:00 +0000

    setup.py updates

--- setup.py
+++ setup.py
@@ -3,24 +3,22 @@ import os.path
 
 from setuptools import find_packages, setup
 
-
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 
 def read(rel_path):
     here = os.path.abspath(os.path.dirname(__file__))
-    with codecs.open(os.path.join(here, rel_path), 'r') as fp:
+    with codecs.open(os.path.join(here, rel_path), "r") as fp:
         return fp.read()
 
 
 def get_version(rel_path):
     for line in read(rel_path).splitlines():
-        if line.startswith('__version__'):
-            delim = '"' if '"' in line else "'"
-            return line.split(delim)[1]
-        else:
+        if not line.startswith("__version__"):
             raise RuntimeError("Unable to find version string.")
+        delim = '"' if '"' in line else "'"
+        return line.split(delim)[1]
 
 
 setup(
@@ -35,7 +33,7 @@ setup(
     include_package_data=True,
     zip_safe=False,
     classifiers=[
-        'Framework :: Django',
-        'Programming Language :: Python :: 3',
-    ]
+        "Framework :: Django",
+        "Programming Language :: Python :: 3",
+    ],
 )

b5ee1b3b8aab931cb15a7d794e69f74bcb718b29
Abhimanyu Saharan <desk.abhimanyu@gmail.com>
2024-01-05 17:06:04 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,15 +1,8 @@
 #!/bin/bash
 # Runs the NetBox plugin unit tests
-# Usage:
-#   ./test.sh latest
-#   ./test.sh v2.9.7
-#   ./test.sh develop-2.10
 
 # exit when a command exits with an exit code != 0
 set -e
-
-# NETBOX_VARIANT is used by `Dockerfile` to determine the tag
-NETBOX_VARIANT="${1-latest}"
 
 # The docker compose command to use
 doco="docker compose --file docker-compose.yml"
@@ -27,12 +20,10 @@ test_cleanup() {
   docker image rm docker.io/library/netbox-acls-netbox || echo ''
 }
 
-export NETBOX_VARIANT=${NETBOX_VARIANT}
-
-echo "🐳🐳🐳  Start testing '${NETBOX_VARIANT}'"
+echo "🐳🐳🐳  Start testing"
 
 # Make sure the cleanup script is executed
 trap test_cleanup EXIT ERR
 test_netbox_unit_tests
 
-echo "🐳🐳🐳  Done testing '${NETBOX_VARIANT}'"
+echo "🐳🐳🐳  Done testing"

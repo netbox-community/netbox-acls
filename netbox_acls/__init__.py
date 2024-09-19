25c8eebb213a0e7c17e07352d7c464c2e924a9bb
Chris Russell <cruse1977123@gmail.com>
2024-09-19 20:31:15 +0100

    Update __init__.py min, max versions

--- netbox_acls/__init__.py
+++ netbox_acls/__init__.py
@@ -17,8 +17,8 @@ class NetBoxACLsConfig(PluginConfig):
     version = __version__
     description = "Manage simple ACLs in NetBox"
     base_url = "access-lists"
-    min_version = "4.0.2"
-    max_version = "4.0.99"
+    min_version = "4.1.0"
+    max_version = "4.1.20"
 
 
 config = NetBoxACLsConfig

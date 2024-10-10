02f428acb0d85a51342eb63b89ba06e44b557f1e
Chris Russell <cruse1977123@gmail.com>
2024-10-10 21:09:11 +0100

    bump version to 1.7.0

--- netbox_acls/__init__.py
+++ netbox_acls/__init__.py
@@ -18,7 +18,7 @@ class NetBoxACLsConfig(PluginConfig):
     description = "Manage simple ACLs in NetBox"
     base_url = "access-lists"
     min_version = "4.1.0"
-    max_version = "4.1.20"
+    max_version = "4.1.99"
 
 
 config = NetBoxACLsConfig

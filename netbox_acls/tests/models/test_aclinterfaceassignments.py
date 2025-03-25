d027cd02b9a7c5f5007e858c7887fa20288160bf
Martin Hauser <git@pheus.dev>
2025-03-25 23:04:58 +0100

    chore(tests): Enhance ACLInterfaceAssignment tests
    Improve test coverage for ACLInterfaceAssignment validation.
    Add tests for valid and invalid direction choices,
    duplicate assignments, and missing host assignments to ensure robust
    validation logic.

--- netbox_acls/tests/models/test_aclinterfaceassignments.py
+++ netbox_acls/tests/models/test_aclinterfaceassignments.py
@@ -65,8 +65,8 @@ class TestACLInterfaceAssignment(BaseTestCase):
 
     def test_acl_interface_assignment_fail(self):
         """
-        Test that ACLInterfaceAssignment passes validation if the ACL is assigned to the host
-        and not already assigned to the vminterface and direction.
+        Test that ACLInterfaceAssignment fails validation if the ACL is not
+        assigned to the parent host.
         """
         device_acl = AccessList(
             name="STANDARD_ACL",
@@ -83,6 +83,7 @@ class TestACLInterfaceAssignment(BaseTestCase):
         )
         with self.assertRaises(ValidationError):
             acl_vm_interface.full_clean()
+            acl_vm_interface.save()
 
     def test_acl_vminterface_assignment_success(self):
         """
@@ -104,20 +105,33 @@ class TestACLInterfaceAssignment(BaseTestCase):
         )
         acl_vm_interface.full_clean()
 
-    def test_acl_host_interface_assignment_fail(self):
-        """
-        Test that ACLInterfaceAssignment fails validation if the ACL is not assigned to the parent host.
-        """
-        pass
-        # TODO: test_acl_interface_assignment_fail - VM & Device
-
     def test_duplicate_assignment_fail(self):
         """
         Test that ACLInterfaceAssignment fails validation
         if the ACL already is assigned to the same interface and direction.
         """
-        pass
-        # TODO: test_duplicate_assignment_fail - VM & Device
+        device_acl = AccessList(
+            name="STANDARD_ACL",
+            assigned_object=self.device1,
+            type="standard",
+            default_action="permit",
+            comments="STANDARD_ACL",
+        )
+        device_acl.save()
+        acl_device_interface1 = ACLInterfaceAssignment(
+            access_list=device_acl,
+            direction="ingress",
+            assigned_object=self.device_interface1,
+        )
+        acl_device_interface1.full_clean()
+        acl_device_interface1.save()
+        acl_device_interface2 = ACLInterfaceAssignment(
+            access_list=device_acl,
+            direction="ingress",
+            assigned_object=self.device_interface1,
+        )
+        with self.assertRaises(ValidationError):
+            acl_device_interface2.full_clean()
 
     def test_acl_already_assigned_fail(self):
         """
@@ -127,4 +141,50 @@ class TestACLInterfaceAssignment(BaseTestCase):
         pass
         # TODO: test_acl_already_assigned_fail - VM & Device
 
-    # TODO: Test choices for ACLInterfaceAssignment Model
+    def test_valid_acl_interface_assignment_choices(self):
+        """
+        Test that ACLInterfaceAssignment action choices using VALID choices.
+        """
+        valid_acl_assignment_direction_choices = ["ingress", "egress"]
+
+        test_acl = AccessList(
+            name="STANDARD_ACL",
+            assigned_object=self.device1,
+            type="standard",
+            default_action="permit",
+            comments="STANDARD_ACL",
+        )
+        test_acl.save()
+
+        for direction_choice in valid_acl_assignment_direction_choices:
+            valid_acl_assignment = ACLInterfaceAssignment(
+                access_list=test_acl,
+                direction=direction_choice,
+                assigned_object=self.device_interface1,
+                comments=f"VALID ACL ASSIGNMENT CHOICES USED: direction={direction_choice}",
+            )
+            valid_acl_assignment.full_clean()
+
+    def test_invalid_acl_choices(self):
+        """
+        Test that ACLInterfaceAssignment action choices using INVALID choices.
+        """
+        invalid_acl_assignment_direction_choice = "both"
+
+        test_acl = AccessList(
+            name="STANDARD_ACL",
+            assigned_object=self.device1,
+            type="standard",
+            default_action="permit",
+            comments="STANDARD_ACL",
+        )
+        test_acl.save()
+
+        invalid_acl_assignment_direction = ACLInterfaceAssignment(
+            access_list=test_acl,
+            direction=invalid_acl_assignment_direction_choice,
+            assigned_object=self.device_interface1,
+            comments=f"INVALID ACL DEFAULT CHOICE USED: default_action='{invalid_acl_assignment_direction_choice}'",
+        )
+        with self.assertRaises(ValidationError):
+            invalid_acl_assignment_direction.full_clean()
